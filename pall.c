7ad0923b395fca46dd43dee0439803021373471c
Nigel Omondi <nigelomondi62@gmail.com>
2023-05-28 16:58:29 +0300

    Print all elements of the stack

--- pall.c
+++ pall.c
@@ -1,13 +1,30 @@
 #include "monty.h"
 
 /**
- * pall - prints the stack
- * @stack: stack given by main in start.c
- * @line_cnt: amount of lines
- *
+ * pall_op - prints all tha values of the stack starting from the top
+ * @my_stack: double pointer to the stack to print
+ * @line_no: line no. associated with pall in the bytecodes
  * Return: void
  */
-void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+void pall_op(stack_t **my_stack, unsigned int line_no __attribute__((unused)))
 {
-	print_stack(*stack);
+	stack_display(*my_stack);
+}
+
+/**
+ * stack_display - displays the values of the stack
+ * @my_stack: the stack to display its values
+ * Return: size of the stack
+ */
+size_t stack_display(const stack_t *my_stack)
+{
+	size_t stack_size = 0;
+
+	while (my_stack)
+	{
+		printf("%d\n", my_stack->n);
+		my_stack = my_stack->next;
+		stack_size++;
+	}
+	return (stack_size);
 }
