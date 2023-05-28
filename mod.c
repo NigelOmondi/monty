3d3860b0644b123e89a20ea10fefbf18d88a34e8
Nigel Omondi <nigelomondi62@gmail.com>
2023-05-28 16:54:10 +0300

    Give modulus of a division

--- mod.c
+++ mod.c
@@ -1,34 +1,29 @@
 #include "monty.h"
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 /**
- * mod - computes the remainder of the division
- * @stack: stack given by main
- * @line_cnt: line counter
- *
+ * mod_op - gives the remainder of division
+ * @my_stack: double pointer to the stack containing the two elements
+ * @line_no: line no associated with the mod opcode
  * Return: void
  */
-void mod(stack_t **stack, unsigned int line_cnt)
+void mod_op(stack_t **my_stack, unsigned int line_no)
 {
-	int result;
+	int mod_result;
 
-	if (!stack || !*stack || !((*stack)->next))
+	if (!my_stack || !*my_stack || !((*my_stack)->next))
 	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", line_cnt);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_no);
 		exit(EXIT_FAILURE);
 		return;
 	}
-	if (((*stack)->n) == 0)
+	if (((*my_stack)->n) == 0)
 	{
-		fprintf(stderr, "L%d: division by zero\n", line_cnt);
+		fprintf(stderr, "L%d: division by zero\n", line_no);
 		exit(EXIT_FAILURE);
 		return;
 	}
 
-	result = ((*stack)->next->n) % ((*stack)->n);
-	pop(stack, line_cnt);/*For top node*/
-	(*stack)->n = result;
+	mod_result = ((*my_stack)->next->n) % ((*my_stack)->n);
+	pop_op(my_stack, line_no);
+	(*my_stack)->n = mod_result;
 }
